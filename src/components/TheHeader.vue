264d74f8e1864937239d7939846dd45ff9042c8c
takashi <takashi@asial.co.jp>
2020-03-26 13:49:05 +0900

    initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="title">
+      Roo
+    </div>
+    <div class="category">
+      <div>AAAAA</div>
+      <div>AAAAA</div>
+      <div>AAAAA</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import Logo from '~/components/Logo.vue'
+// import Header from '~/componets/Header.vue'
+
+// interface User {
+//   name: string
+// }
+
+// export type DataType = {
+//   name: string
+//   cost: number
+// }
+
+export default Vue.extend({
+  data() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.header {
+  .title {
+    display: inline-block;
+    height: 20%;
+    width: 100%;
+  }
+
+  .category {
+    display: flex;
+  }
+}
+</style>
